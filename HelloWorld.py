a2acf1e19082a9fb10b365e2197c0ee2cb479ce9
eeevily <yuying@hackthehood.org>
2021-06-22 16:54:28 -0700

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+name = 'LeBron' # string
+age = 35 # integer
+salary = 37.44 #float
+plays_basketball = True #boolean
+jersey_number = '23' #string
+
+# STEP 2 #
+car_name = 'Tricycle'
+x = 50
+my_first_name = "Lily"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
