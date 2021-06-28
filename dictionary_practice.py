0a186bd492f7bf14765cbb6b4c471e927f99d1ed
eeevily <yuying@hackthehood.org>
2021-06-28 16:54:45 -0700

    second commit

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -1,9 +1,9 @@
 # STEP 2 #
-dict_food = {"chicken":1.59, "beef":1.99, "cheese":1.00, "milk":2.50}
+food = {"chicken":1.59, "beef":1.99, "cheese":1.00, "milk":2.50}
 
 # STEP 3 #
 Colors = {"green":1, "blue":2, "red":3, "pink":4}
-Cheapest_food = dict_food["cheese"]
+Cheapest_food = food["cheese"]
 Favorite_colors = Colors["green"]
 
 # STEP 4 - 5 #
@@ -29,9 +29,47 @@ shoe_counts["Jordan_11"] = 9
 
 print(shoe_counts)
 
-#STEP 9 #
+# STEP 9 #
 
 del shoe_counts["Jordan_11"]
 del shoe_counts["Jordan_4"]
 
 print(shoe_counts)
+
+# LAB 4 Part 2 #
+
+def total_price(food1, food2):
+    total = food[food1] + food[food2]
+    return total
+
+
+goodToGo = True 
+while (goodToGo):
+    input1 = input("pls give us a food")
+    if (input1 in food):
+        print("ok")
+        input2 = input("give us a 2nd food")
+    if (input2 in food):
+            print ("the total price is", total_price("beef","cheese"))
+            break
+else:
+    goodToGo = False
+
+# LAB 4 Part 3 #
+
+def clearanceSale(d,updater):
+  d1 = {"Jordan_13":shoe_counts["Jordan_13"]/updater}
+  d.update(d1)
+  d2 = {"Yeezy":shoe_counts["Yeezy"]/updater}
+  d.update(d2)
+  d3 = {"Foamposite":d["Foamposite"]/updater}
+  d.update(d3)
+  d4 = {"Air_Max":d["Air_Max"]/updater}
+  d.update(d4)
+  d5 = {"SB_Dunk":d["SB_Dunk"]/updater}
+  d.update(d5)
+  return (shoe_counts)
+    
+
+print(clearanceSale(shoe_counts,5))
+    
--- lab_4.py
+++ lab_4.py
@@ -1,7 +1,8 @@
 # STEP 1-7 #
 
 def rectangleArea(base,height):
-    area = base*height
+    area = ((base*height)/2)
     return area
 
-print(rectangleArea(4,4))
+print(rectangleArea(3,4))
+print(rectangleArea(8,11))
